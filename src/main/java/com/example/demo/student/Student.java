d8d72c396ac5c5548058411ea0f9fbc663534c57
Hannan Isa <taohannan@yahoo.com>
2022-12-29 22:03:58 +0800

    Initial Commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,15 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+}
